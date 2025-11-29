cda18c2a8b78dc9855382cffff136096282f4547
Quang Dung <dunglele0309@gmail.com>
2025-11-29 20:46:35 +0700

    Sao lưu _Models

---
+++ Smarts_DoAn_Backup_27_11_2025/Models/CheckoutList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smarts_DoAn_Backup_27_11_2025.Models
+{
+    public class CheckoutList
+    {
+        public IEnumerable<DATHANG> DatHangs { get; set; }
+        public IEnumerable<HOADON> HoaDons { get; set; }
+        public IEnumerable<CHITIETHOADON> ChiTietHoaDons { get; set; }
+    }
+}
---
+++ Smarts_DoAn_Backup_27_11_2025/Models/ProductListVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Smarts_DoAn_Backup_27_11_2025.Models
+{
+    public class ProductListVM
+    {
+        public IEnumerable<SANPHAM> SanPhams { get; set; }
+        public IEnumerable<DANHMUC> DanhMucs { get; set; }
+    }
+}
